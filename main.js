caec8ccca44f6b8aaaf747cf5d91b0477dbb1e53
Sarah Reid <sreid@covermymeds.com>
2017-02-02 08:04:28 -0500

    electron-reload needs to be in dependencies to run when packaged

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ require('electron-reload')(__dirname);
 const electron = require('electron')
 // Module to control application life.
 const app = electron.app
+
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
